d46cd4d8a2c7868d867e618ab6677bf7c28edf34
mbougar <146754886+mbougar@users.noreply.github.com>
2024-10-22 15:47:23 +0200

    Update Main.java

--- src/main/java/com/es/Main.java
+++ src/main/java/com/es/Main.java
@@ -3,12 +3,15 @@ package com.es;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Estos son loes ejercicios en Java.");
+        System.out.println("Estos son los ejercicios en Java.");
 
         System.out.println("\nEjercicio2:");
         Ejercicios.ejercicio2();
 
         System.out.println("\nEjercicio4:");
         Ejercicios.ejercicio4();
+
+        System.out.println("\nEjercicio6:");
+        Ejercicios.ejercicio6();
     }
 }
